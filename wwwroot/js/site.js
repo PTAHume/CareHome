a280455f7702994e49392b79c23e2d808d8ea838
PTAHume <102745939+PTAHume@users.noreply.github.com>
2022-07-25 15:03:22 +0100

    ux fixes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,36 +10,43 @@
         width: '80%',
     });
 
-    let $table = $(".tableSorter"),
-        // define pager options
-        pagerOptions = {
-            // target the pager markup - see the HTML block below
-            container: $(".pager"),
-            // output string - default is '{page}/{totalPages}';
-            // possible variables: {size}, {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
-            // also {page:input} & {startRow:input} will add a modifiable input in place of the value
-            output: '{startRow} - {endRow} / {filteredRows} ({totalRows})',
-            // if true, the table will remain the same height no matter how many records are displayed. The space is made up by an empty
-            // table row set to a height to compensate; default is false
-            fixedHeight: true,
-            // remove rows from the table to speed up the sort of large tables.
-            // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
-            removeRows: false,
-            // go to page selector - select dropdown that sets the current page
-            cssGoto: '.gotoPage'
-        };
+    let pagerOptions = {
+        // target the pager markup - see the HTML block below
+        container: $(".pager"),
+        // output string - default is '{page}/{totalPages}';
+        // possible variables: {size}, {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
+        // also {page:input} & {startRow:input} will add a modifiable input in place of the value
+        output: '{startRow} - {endRow} / {filteredRows} ({totalRows})',
+        // if true, the table will remain the same height no matter how many records are displayed. The space is made up by an empty
+        // table row set to a height to compensate; default is false
+        fixedHeight: false,
+        // remove rows from the table to speed up the sort of large tables.
+        // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
+        removeRows: false,
+        // go to page selector - select dropdown that sets the current page
+        cssGoto: '.gotoPage'
+    };
 
-    $table
-        .tablesorter({
-            theme: 'blue',
-            headerTemplate: '{content} {icon}', // new in v2.7. Needed to add the bootstrap icon!
-            widthFixed: true,
-            widgets: ['zebra', 'filter'],
-        })
 
-        // initialize the pager plugin
-        // ****************************
-        .tablesorterPager(pagerOptions);
+    $(".tableSorter").tablesorter({
+        theme: 'jui',
+        showProcessing: true,
+        headerTemplate: '{content} {icon}',
+        widgets: ['uitheme', 'zebra', 'filter', 'scroller'],
+        widgetOptions: {
+            scroller_height: 300,
+            // scroll tbody to top after sorting
+            scroller_upAfterSort: true,
+            // pop table header into view while scrolling up the page
+            scroller_jumpToHeader: true,
+            // In tablesorter v2.19.0 the scroll bar width is auto-detected
+            // add a value here to override the auto-detected setting
+            scroller_barWidth: null
+            // scroll_idPrefix was removed in v2.18.0
+            // scroller_idPrefix : 's_'
+        }
+    }).tablesorterPager(pagerOptions);
+
 
     $(".datepicker").datepicker({
         value: new Date(2013, 10, 10),
@@ -73,10 +80,6 @@
         $("body").removeClass("Index");
     }
 
-    if ($(".tablesorter-ignoreRow").length > 0) {
-        $(".tablesorter-ignoreRow").find("td")[0].innerHTML = "Filters" + $(".tablesorter-ignoreRow").find("td")[0].innerHTML;
-    }
-
     $(".DisplayContactDetails").on("click", function (e) {
         e.preventDefault
         $(".modal").dialog("close");
@@ -88,7 +91,9 @@
         $("div").find(`[data-address-details-table='${$(this).attr('data-id')}']`).dialog("open");
     });
     $("#Department").change(function () {
-        $.getJSON("/Staff/GetJobList", { DepartmentId: parseInt($("#Department").val()) }, function (data) {
+        $.getJSON("/Staff/GetJobList", {
+            DepartmentId: parseInt($("#Department").val())
+        }, function (data) {
             let row = "";
             $("#JobTitle").empty();
             $.each(data, function (index, item) {
